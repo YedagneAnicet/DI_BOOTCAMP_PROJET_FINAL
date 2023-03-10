b7a025d6b8de8cdb58beb975023f2e49c06b4ca0
Yedagne Mel Ange Anicet <yedagneanicet@gmail.com>
2023-03-10 10:45:56 +0000

    projet final

--- fontend_projet_final_di/src/app/app.module.ts
+++ fontend_projet_final_di/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductComponent } from './components/product/product.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { ProductComponent } from './components/product/product.component';
     HomeComponent,
     HeaderComponent,
     ProductComponent,
+    BlogComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
