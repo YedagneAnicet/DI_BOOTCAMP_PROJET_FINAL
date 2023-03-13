0be7bfc9e3575370dd0eaaf9c9f71ae1177ff013
Yedagne Mel Ange Anicet <yedagneanicet@gmail.com>
2023-03-13 13:43:37 +0000

    table pharmacie mis a jour

--- backend_projet_final_di/public/index.php
+++ backend_projet_final_di/public/index.php
@@ -194,6 +194,200 @@ $app->post('/api/utilisateurs/deconnexion', function (Request $request, Response
 
 
 
+//ACTION SUR LA TABLE PHARMACIE 
+
+
+//ajouter une nouvelle pharmacie 
+
+$app->post('/api/pharmacies_add', function (Request $request, Response $response, array $args) {
+
+    $data = $request->getParsedBody();
+    
+    $nom = $data['nom_pharmacie'];
+    $adresse = $data['adresse_pharmacie'];
+    $ville = $data['ville_pharmacie'];
+    $telephone = $data['telephone_pharmacie'];
+    $email = $data['email_pharmacie'];
+    $role = $data['role'];
+    $garde = $data['de_garde'];
+
+    // Vérification des paramètres obligatoires
+    if (!isset($nom) || !isset($adresse) || !isset($ville) || !isset($telephone) || !isset($email)) {
+        $error = array(
+            "message" => "Tous les champs obligatoires doivent être fournis"
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(400);
+    }
+
+    if(!isset($role)){
+        $role = 0;
+    }
+
+    if(!isset($garde)){
+        $garde = 0;
+    }
+
+    $sql = "INSERT INTO pharmacies (nom_pharmacie, adresse_pharmacie, ville_pharmacie, telephone_pharmacie, email_pharmacie, role, de_garde) VALUES (:nom_pharmacie, :adresse_pharmacie, :ville_pharmacie, :telephone_pharmacie, :email_pharmacie, :role, :de_garde)";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':nom_pharmacie', $nom);
+        $stmt->bindParam(':adresse_pharmacie', $adresse);
+        $stmt->bindParam(':ville_pharmacie', $ville);
+        $stmt->bindParam(':telephone_pharmacie', $telephone);
+        $stmt->bindParam(':email_pharmacie', $email);
+        $stmt->bindParam(':role', $role);
+        $stmt->bindParam(':de_garde', $garde);
+
+        $stmt->execute();
+        $lastInsertId = $conn->lastInsertId();
+        $db = null;
+
+        $responseBody = array(
+            "id_pharmacie" => $lastInsertId,
+            "message" => "Pharmacie ajoutée avec succès"
+        );
+
+        $response->getBody()->write(json_encode($responseBody));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(201);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+
+//selectionner la liste de toute les pharmacie
+
+$app->get('/api/pharmacies/all', function (Request $request, Response $response, array $args) {
+
+    $sql = "SELECT * FROM pharmacies";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->query($sql);
+        $pharmacies = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        $response->getBody()->write(json_encode($pharmacies));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+
+//selection une pharmacie avec son id 
+
+$app->get('/api/pharmacies/{id}', function (Request $request, Response $response, array $args) {
+
+    $pharmacyId = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM pharmacies WHERE id_pharmacie = :id_pharmacie";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id_pharmacie', $pharmacyId);
+        $stmt->execute();
+        $pharmacy = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+
+        if ($pharmacy) {
+            $response->getBody()->write(json_encode($pharmacy));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } else {
+            $error = array(
+                "message" => "La pharmacie est introuvable"
+            );
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(404);
+        }
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+
+//selectionner la liste de tous les pharmacie de garde 
+
+$app->get('/api/pharmacies_garde', function (Request $request, Response $response, array $args) {
+
+    $sql = "SELECT * FROM pharmacies WHERE de_garde = 1";
+
+    try {
+        $db = new DB();
+        $conn = $db->connect();
+        $stmt = $conn->query($sql);
+        $pharmacies = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        $response->getBody()->write(json_encode($pharmacies));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+
+
+
+
+
+
+
+
+//FIN TABLE PHARMACIE
+
+
+
+
+
 //FIN DE L'API
 
 
